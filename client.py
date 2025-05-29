fc92d0648faa6d50d3fd8aa99c2f2c4012af927a
fatemeh-khajeh <72465357+fatemeh-khajeh@users.noreply.github.com>
2025-05-29 11:44:54 -0400

    client.py updated
    client.py updated for using azure

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 # client.py
 import requests
 
-url = 'http://localhost:8000/test'
-data = {'value': ''}
-response = requests.get(url,data)
+#url = 'http://localhost:8000/test'
+url = 'https://rcw-main-gzdya8fncpafgfdg.canadaeast-01.azurewebsites.net/'
+
+response = requests.get(url)
 response = response.json()
 print(response['message'])
 
